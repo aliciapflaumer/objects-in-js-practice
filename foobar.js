4597155dc6147c74d885b47732ec9f41240740eb
aliciapflaumer <alpfla7@gmail.com>
2018-06-28 20:12:04 -0400

    Created new js file for foobar issue
    - Able to use `Object.keys` to get key

---
+++ foobar.js
@@ -0,0 +1,33 @@
+// Object in Object
+
+const data = {
+  "pages": [
+    {
+      "address":"http://foo.bar.com/p1",
+      "links": ["http://foo.bar.com/p2", "http://foo.bar.com/p3", "http://foo.bar.com/p4"]
+    }
+  ]
+}
+
+const data1 = {
+  "address":"http://foo.bar.com/p1",
+  "links": ["http://foo.bar.com/p2", "http://foo.bar.com/p3", "http://foo.bar.com/p4"]
+}
+
+
+let result = ""
+let keys1 = Object.keys(data1)
+// let values = Object.values(data1)
+
+for (let unique in data1) {
+  result = result + data1[unique] + " "
+}
+
+// console.log(result)
+// outputs: `http://foo.bar.com/p1 http://foo.bar.com/p2,http://foo.bar.com/p3,http://foo.bar.com/p4`
+
+// console.log(keys1)
+// outputs: `[ 'address', 'links' ]`
+
+
+// console.log(values)

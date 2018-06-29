725ac19a08ce5c23dd216061d3769527442a2272
aliciapflaumer <alpfla7@gmail.com>
2018-06-28 22:40:48 -0400

    Able to manually step through first object
    - Next steps, loop through

--- foobar.js
+++ foobar.js
@@ -28,30 +28,33 @@ const data = {
 const data1 = {
   "address":"http://foo.bar.com/p1",
   "links": ["http://foo.bar.com/p2", "http://foo.bar.com/p3", "http://foo.bar.com/p4"]
-},
-{
-  "address":"http://foo.bar.com/p2",
-  "links": ["http://foo.bar.com/p2", "http://foo.bar.com/p4"]
-},
-{
-  "address":"http://foo.bar.com/p4",
-  "links": ["http://foo.bar.com/p5", "http://foo.bar.com/p1", "http://foo.bar.com/p6"]
-},
-{
-  "address":"http://foo.bar.com/p5",
-  "links": []
-},
-{
-  "address":"http://foo.bar.com/p6",
-  "links": ["http://foo.bar.com/p7", "http://foo.bar.com/p4", "http://foo.bar.com/p5"]
 }
+// {
+//   "address":"http://foo.bar.com/p2",
+//   "links": ["http://foo.bar.com/p2", "http://foo.bar.com/p4"]
+// },
+// {
+//   "address":"http://foo.bar.com/p4",
+//   "links": ["http://foo.bar.com/p5", "http://foo.bar.com/p1", "http://foo.bar.com/p6"]
+// },
+// {
+//   "address":"http://foo.bar.com/p5",
+//   "links": []
+// },
+// {
+//   "address":"http://foo.bar.com/p6",
+//   "links": ["http://foo.bar.com/p7", "http://foo.bar.com/p4", "http://foo.bar.com/p5"]
+// }
 
-let string = ""
-let keys = Object.keys(data)
-let values = Object.values(data)
+// let string = ""
+// let keys = Object.keys(data)
+// let values = Object.values(data)
 
-let combo = Object.entries(data)
-console.log(combo)
+let address = Object.entries(data1)[0][1]
+let links = Object.entries(data1)[1][1][0]
+console.log(address) // outputs: `http://foo.bar.com/p1`
+console.log(links) // outputs: `http://foo.bar.com/p2`
+
 
 // for (let property in data1) {
 //   string = string + data1[property] + " "
